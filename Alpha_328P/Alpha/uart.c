4135ea1d4da1a985108881d3338df9352c3d48f2
Xennis <xennis@online.de>
2012-11-30 11:48:14 +0100

    Atmel Studio project
    Send and receive (interrupt driven) works; Septup do not work

---
+++ Alpha_328P/Alpha/uart.c
@@ -0,0 +1,50 @@
+/*
+ * uart.c
+ *
+ * Created: 30.10.2012 10:27:18
+ * Author: Fabi & Florian
+ */ 
+#include "uart.h"
+
+/* 
+ * USART Initialization (Datasheet page 178)
+ */
+void uart_init( unsigned int ubrr )
+{
+	/* Set baud rate */
+	UBRRH = (unsigned char)(ubrr>>8);
+	UBRRL = (unsigned char)ubrr;
+	
+	UCSRA = (0<<U2X0);
+	/* Enable receiver, transmitter and UART RX Complete Interrupt (activate global interrupt flag necessary) */
+	UCSRB |= (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0);
+	/* Set frame format: 8data, 1stop bit */
+	UCSRC |= (1<<UCSZ01)|(1<<UCSZ00);
+}
+
+/*
+ * Sending Frames with 5 to 8 Data Bit (Datasheet page 179)
+ */
+void uart_putc( unsigned char data )
+{
+	/* Wait for empty transmit buffer */
+	while ( !( UCSRA & (1<<UDRE) ) );
+	/* Put data into buffer, sends the data */
+	UDR = data;
+}
+
+void uart_timer_init( void ) {
+	DDRD |= (1<<DDD6) | (1<<DDD5);
+	// normale 8-bit PWM aktivieren (nicht invertiert),
+	// Das Bit WGM10 wird im Datenblatt auch als PWM10 bezeichnet
+	TCCR0A = (1<<COM0A0)|(1<<COM0A1)|(1<<COM0B0)|(1<<COM0B1)|(1<<WGM00);
+
+	// Einstellen der PWM-Frequenz auf 14 kHz (Prescaler = 1)
+	TCCR0B = (1<<CS00);
+	TCNT0=255;
+	OCR0A=255;
+	OCR0B=235;
+	// Interrupts für Timer1 deaktivieren
+	// Achtung: Auch die Interrupts für die anderen Timer stehen in diesem Register
+	// TIMSK &= ~0x3c;
+}
